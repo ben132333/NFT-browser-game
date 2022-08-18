88195b091258e63690d77b6a6766e54b79d7e950
ben132333 <ben.dumoulin13@gmail.com>
2022-08-17 19:43:21 -0500

    created new directory

---
+++ replit-frontend/src/constants.jsx
@@ -0,0 +1,13 @@
+const CONTRACT_ADDRESS = '0xd08224fd0898d37DDa12B2B1fA7521d34b7420Ba';
+
+const transformCharacterData = (characterData) => {
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
+  };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
